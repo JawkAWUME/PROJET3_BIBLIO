748cac5de299d7af07d374c1859413e1853fb0d1
JawkAWUME <109081623+JawkAWUME@users.noreply.github.com>
2022-11-27 22:27:30 +0000

    Update models3.php

--- PROJET/models3.php
+++ PROJET/models3.php
@@ -1,29 +1,56 @@
 <?php
-function find_auteur(int $id):array{
-    $auteurs=[
-        ["idA"=>1,"nomA"=>"CLARK","prenomA"=>"James","profession1"=>"Ecrivain,"."  <br>Philosophe"],
-        ["idA"=>2,"nomA"=>"JAMES","prenomA"=>"LeBron","profession1"=>"Basketteur,"."  <br>Entrepreneur"],
-        ["idA"=>3,"nomA"=>"WALLACE","prenomA"=>"Christopher","profession1"=>"Ecrivain"],
-        ["idA"=>4,"nomA"=>"CLOSIYI","prenomA"=>"Temilade","profession1"=>"Chanteuse Soul,"." "." <br>Compositrice"],
-        ];
-    foreach($auteurs as $k =>$v){
-       if($k="idA"){
-        if($v[$k] == $id){
+require_once("data.projet3.php");
+function find_rayon(int $id):array|null{
+    $rayons=all_rayons();
+    foreach($rayons as $k=> $v){
+        if($v["id"] == $id){
             return $v;
         }
-    }
 }
 }
-
-function ouvrage(array $ouvrages):array{
-    $ouvro=[];
-    foreach($ouvrages as $k=> $v){
-        $ouvro=find_auteur($ouvrages["auteur_id"]);
-        $ouvr[$k]=[
+function find_auteur(int $id):array|null{
+    $auteurs=all_authors();
+    foreach($auteurs as $k=> $v){
+        if($v["idA"] == $id){
+            return $v;
+        }
+}
+return null;
+}
+function find_ouvrage(int $id):array{
+        $ouvrages=all_ouvrages();
+        foreach($ouvrages as $k =>$v){
+            if($v["codeO"] == $id){
+                return $v;
+            }
+}
+}
+function ouvrage():array{
+    $ouvrages=all_ouvrages();
+    $ouvr=[];
+    foreach($ouvrages as $k => $v){
+        $ouv=find_auteur($v["auteur_id"]);
+        
+        $ouvr[]=[
             "id"=>$v["codeO"],
             "titre"=>$v["titre"],
             "date"=>$v["dateE"],
-            "auteur"=>$ouvro["prenomA"]."  ".$ouvro["nomA"]
+            "auteur"=>$ouv["prenomA"]."  ".$ouv["nomA"]
+        ];
+    }
+    return $ouvr;
+}
+function exemplaire():array{
+    $exemplaires=all_exemplaires();
+    $ouvr=[];
+    foreach($exemplaires as $k=> $v){
+        $ouv=find_ouvrage($v["ouvrage_id"]);
+        $ex=find_rayon($v["rayon_id"]);
+        $ouvr[]=[
+            "id"=>$v["codeE"],
+            "date"=>$v["dateE"],
+            "ouvrage"=>$ouv["titre"],
+            "rayon"=>$ex["nom"],
         ];
     }
     return $ouvr;
