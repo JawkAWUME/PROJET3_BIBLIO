7489d37ce58ecb282c3357fc7ca3d85fa295c34d
JawkAWUME <109081623+JawkAWUME@users.noreply.github.com>
2022-11-27 23:08:34 +0000

    Update liste.exemplaires.php

--- PROJET/liste.exemplaires.php
+++ PROJET/liste.exemplaires.php
@@ -1,17 +1,22 @@
-<?php require_once("data.projet3.php");
-    $exempl=exemplaire($exemplaires);?>
+
     <div class="container">
         <table>
+            <caption>LISTE DES EXEMPLAIRES</caption>
             <tr>
             <th>ID</th>
            <th>DATE</th>
-           <th></th>
+           <th>OUVRAGES</th>
+           <th>RAYON</th>
             </tr>
-        <?php foreach ($exempl as $v):?>
+        <?php 
+          $exempl=exemplaire();
+        foreach ($exempl as $k  => $v):?>
             <tr>
                 <th><?php echo($v["id"]);?></th>
                 <th><?php echo($v["date"]);?></th>
                 <th><?php echo($v["ouvrage"]);?></th>
+                <th><?php echo($v["rayon"]);?></th>
+
             </tr>
         <?php endforeach ?>
         </table>
